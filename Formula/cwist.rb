7b3a666d9ed6c2d19dc860c87fe5047cc2445d0f
Lee Yunjin <gzblues61@daum.net>
2026-09-25 10:37:22 +0900

    cwist 3.7

--- Formula/cwist.rb
+++ Formula/cwist.rb
@@ -8,8 +8,8 @@
 class Cwist < Formula
   desc "C17 web framework and application server (HTTP/1.1, HTTP/2, HTTP/3, WebSocket, PQC TLS)"
   homepage "https://github.com/c4punks/CWIST"
-  url "https://github.com/c4punks/CWIST/releases/download/v3.6/cwist-3.6.tar.gz"
-  sha256 "d003d9f7d8e6af8cbca4e80628c4799c96da6f4598fc0569a347ffb1c24c1414"
+  url "https://github.com/c4punks/CWIST/releases/download/v3.7/cwist-3.7.tar.gz"
+  sha256 "789c8c2b6d90ba07cc8a0d365fe6b8611cddae795b597ee00680cb83d70399a3"
   # The framework itself is MIT; the vendored components it statically links
   # are Apache-2.0 (BoringSSL, cnats), MIT (lsquic, cJSON,
   # multipart-parser-c), BSD-3-Clause (libttak, uriparser, lsquic's Chromium
@@ -25,8 +25,8 @@ class Cwist < Formula
   uses_from_macos "curl"
 
   def install
-    system "make", "-j#{ENV.make_jobs}", "VERSION=3.6"
-    system "make", "install", "PREFIX=#{prefix}", "VERSION=3.6"
+    system "make", "-j#{ENV.make_jobs}", "VERSION=3.7"
+    system "make", "install", "PREFIX=#{prefix}", "VERSION=3.7"
   end
 
   test do
